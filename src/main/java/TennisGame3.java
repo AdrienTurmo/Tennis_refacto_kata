9bc520e4c8b866ace155653dcfbd8f280e1bb4af
Adrien Turmo <adrien.turmo@gmail.com>
2017-07-25 10:52:33 +0200

    TennisGame3 : rename variables and fields

--- src/main/java/TennisGame3.java
+++ src/main/java/TennisGame3.java
@@ -1,34 +1,36 @@
+import java.util.Objects;
+
 public class TennisGame3 implements TennisGame {
 
-    private int p2;
-    private int p1;
-    private String p1N;
-    private String p2N;
+    private int player1score;
+    private int player2score;
+    private String player1name;
+    private String player2name;
 
-    public TennisGame3(String p1N, String p2N) {
-        this.p1N = p1N;
-        this.p2N = p2N;
+    public TennisGame3(String player1name, String player2name) {
+        this.player1name = player1name;
+        this.player2name = player2name;
     }
 
     public String getScore() {
-        String s;
-        if (p1 < 4 && p2 < 4) {
-            String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
-            s = p[p1];
-            return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
+        String tennisScore;
+        if (player1score < 4 && player2score < 4) {
+            String[] tennisScores = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
+            tennisScore = tennisScores[player1score];
+            return (player1score == player2score) ? tennisScore + "-All" : tennisScore + "-" + tennisScores[player2score];
         } else {
-            if (p1 == p2)
+            if (player1score == player2score)
                 return "Deuce";
-            s = p1 > p2 ? p1N : p2N;
-            return ((p1-p2)*(p1-p2) == 1) ? "Advantage " + s : "Win for " + s;
+            tennisScore = player1score > player2score ? player1name : player2name;
+            return ((player1score - player2score)*(player1score - player2score) == 1) ? "Advantage " + tennisScore : "Win for " + tennisScore;
         }
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            this.p1 += 1;
+        if (Objects.equals(playerName, player1name))
+            this.player1score += 1;
         else
-            this.p2 += 1;
+            this.player2score += 1;
 
     }
 
