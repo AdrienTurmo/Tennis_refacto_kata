63d8820c50082935368fe24f931a23e6bffc1dde
Adrien Turmo <adrien.turmo@gmail.com>
2017-07-25 11:36:18 +0200

    TennisGame3 : remove player2name and player2score fields

--- src/main/java/TennisGame3.java
+++ src/main/java/TennisGame3.java
@@ -6,28 +6,25 @@ import java.util.Objects;
 public class TennisGame3 implements TennisGame {
 
     private static final int FORTY = 4;
+    private Player player2;
     private Player player1;
-
-    private int player2points;
-    private String player2name;
 
     public TennisGame3(String player1name, String player2name) {
         player1 = new Player(player1name);
-
-        this.player2name = player2name;
+        player2 = new Player(player2name);
     }
 
     public String getScore() {
         if (bothPlayerAreUnderForty()) {
             String player1tennisScore = pointToTennisScore(player1.getPoints());
-            String player2tennisScore = pointToTennisScore(player2points);
+            String player2tennisScore = pointToTennisScore(player2.getPoints());
             return thereIsADraw() ? (player1tennisScore + "-All") : (player1tennisScore + "-" + player2tennisScore);
         } else {
             if (thereIsADraw()) {
                 return "Deuce";
             }
             String playerWithHighestScore;
-            playerWithHighestScore = player1.getPoints() > player2points ? player1.getName() : player2name;
+            playerWithHighestScore = player1.getPoints() > player2.getPoints() ? player1.getName() : player2.getName();
             return scoreDifferenceIsOne() ? "Advantage " + playerWithHighestScore : "Win for " + playerWithHighestScore;
         }
     }
@@ -37,23 +34,23 @@ public class TennisGame3 implements TennisGame {
             player1.scored();
         }
         else {
-            this.player2points += 1;
+            player2.scored();
         }
     }
 
     private boolean scoreDifferenceIsOne() {
-        return Math.abs(player1.getPoints() - player2points) == 1;
+        return Math.abs(player1.getPoints() - player2.getPoints()) == 1;
     }
 
     private boolean thereIsADraw() {
-        return player1.getPoints() == player2points;
+        return player1.getPoints() == player2.getPoints();
     }
 
     private String pointToTennisScore(int playerScore) {
         return TennisScore.tennisScoreOf(playerScore);
     }
     private boolean bothPlayerAreUnderForty() {
-        return player1.getPoints() < FORTY && player2points < FORTY;
+        return player1.getPoints() < FORTY && player2.getPoints() < FORTY;
     }
 
 
